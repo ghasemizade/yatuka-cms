c379eff2a268597850c4a094103320e296f4052a
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2024-04-11 19:15:03 +0330

    update feature

--- components/toggleModal.js
+++ components/toggleModal.js
@@ -8,5 +8,13 @@ function closeModal() {
 
 function openModal() {
     modalElem.classList.remove('hidden')
+    modalElem.classList.add('flex')
     layerElem.classList.remove('hidden')
 }
+
+document.addEventListener('keyup', (ev) => {
+    if (ev.key === "Escape") {
+        modalElem.classList.add('hidden')
+        layerElem.classList.add('hidden')
+    }
+})
