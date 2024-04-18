bcbf3f0654addfbd551e92cc49fa4020e559320e
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2024-04-18 21:42:43 +0330

    delete user from server

---
+++ components/deletUser.js
@@ -0,0 +1,8 @@
+const modalDeleteUser = document.querySelector('#popup-modal')
+const getRemoveBtn = document.querySelector('#remove-btn')
+
+console.log(getRemoveBtn);
+
+getRemoveBtn.addEventListener('click', () => {
+    console.log('test');
+})
