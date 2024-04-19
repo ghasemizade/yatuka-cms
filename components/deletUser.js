e5c552cd9f2edd8650d2a4fca2480765c63f7fd2
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2024-04-19 13:17:33 +0330

    feat (delete user dom)

--- components/deletUser.js
+++ components/deletUser.js
@@ -2,8 +2,10 @@ const modalDeleteUser = document.querySelector('#popup-modal')
 const getRemoveBtn = document.querySelector('#remove-btn')
 const getLayer = document.querySelector('#layer')
 
+let mainUserId
 
-function showModal () {
+function showModal (userId) {
+    mainUserId = userId
     modalDeleteUser.classList.remove('hidden')
     modalDeleteUser.classList.add('flex')
     getLayer.classList.remove('hidden')
@@ -13,4 +15,13 @@ function closeModal () {
     modalDeleteUser.classList.add('hidden')
     modalDeleteUser.classList.remove('flex')
     getLayer.classList.add('hidden')
+}
+
+function deleteUser () {
+    fetch(`http://localhost:3000/api/users/${mainUserId}`, {
+        method: 'DELETE'
+    }).then(res => {
+        closeModal()
+        getUser()
+    })
 }
