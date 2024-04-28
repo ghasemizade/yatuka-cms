36982be1b584c8811e508e43a807523ea65efc9f
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2024-04-28 23:05:51 +0330

    feat (update info): with PUT method

--- components/info.js
+++ components/info.js
@@ -2,7 +2,9 @@ const getFnamInput = document.querySelector('#fname')
 const getLnamInput = document.querySelector('#lname')
 const getUsernameInput = document.querySelector('#username')
 const getEmailInput = document.querySelector('#email')
-
+const getPasswordInput = document.querySelector('#password')
+const getNewPasswordInput = document.querySelector('#new-password') 
+const getConfirmPasswordInput = document.querySelector('#confirm-password') 
 
 window.addEventListener('load', () => {
     let LoginAdminId = localStorage.getItem('loginId')
@@ -17,3 +19,30 @@ window.addEventListener('load', () => {
         getEmailInput.value = mainAdmin.email
     })
 })
+
+function updateInformation() {
+
+    if (getNewPasswordInput.value === getConfirmPasswordInput.value) {
+        let mainAdminId = localStorage.getItem('loginId')
+        let newInfoAdmin = {
+            firstName: getFnamInput.value,
+            lastName: getLnamInput.value,
+            userName: getUsernameInput.value,
+            password: getConfirmPasswordInput.value,
+            email: getEmailInput.value
+        }
+
+        fetch(`http://localhost:3000/api/admins/${mainAdminId}`, {
+            method: 'PUT',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(newInfoAdmin)
+        })
+        .then(res => console.log(res))
+        
+    } else {
+        alert("new password it is not the same !!!")
+    }
+
+}
